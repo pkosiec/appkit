8634dd12d9c7e172e0126f6912d09b031ca3f855
Pawel Kosiec <pawel.kosiec@databricks.com>
2026-01-13 21:23:17 +0100

    Adjust GH pages build for fork

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  url: "https://databricks.github.io",
+  url: "https://pkosiec.github.io",
   baseUrl: "/appkit/",
 
   organizationName: "databricks",
